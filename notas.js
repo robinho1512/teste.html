94cc23eba533cc43e12075d1a47177b4a3ad5e8e
joaopedro <goncalves.oliveira15@escola.pr.gov.br>
2026-02-24 08:35:17 -0300

    Add files via upload

--- notas.js
+++ notas.js
@@ -1,9 +1,15 @@
-aluno= "pedro";
-nota1=7;
-nota2=8;
-nota3=10;
-nota4=3;
-console.log(media=nota1+nota2+nota3+nota4= x)
-console.log(media=media/4=y)
-x-y=z
-console.log("o aluno")
+// Declaração das variáveis
+let aluno = "Pedro";
+let nota1 = 7;
+let nota2 = 8;
+let nota3 = 10;
+let nota4 = 3;
+
+let soma = nota1 + nota2 + nota3 + nota4;
+
+let media = soma / 4;
+
+console.log("Aluno: " + aluno);
+console.log("Notas: " + nota1 + ", " + nota2 + ", " + nota3 + ", " + nota4);
+console.log("Soma das notas: " + soma);
+console.log("Média: " + media);
