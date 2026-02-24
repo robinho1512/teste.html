8a08592556d5630a222a2c8b320c48130a09ab88
joaopedro <goncalves.oliveira15@escola.pr.gov.br>
2026-02-24 08:32:13 -0300

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,9 @@
+carro1= Belina
+km=42
+valor combustibvel=6,30
+console.log(km+valor combustibvel)
+
+carro2= zafira
+km= 89
+valorcombustivel=8,90
+console.log()           
---
+++ notas.js
@@ -0,0 +1,9 @@
+aluno= "pedro";
+nota1=7;
+nota2=8;
+nota3=10;
+nota4=3;
+console.log(media=nota1+nota2+nota3+nota4= x)
+console.log(media=media/4=y)
+x-y=z
+console.log("o aluno")
