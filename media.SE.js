b0fb22628e895a69eb68eacd7137344290c705ea
joaopedro <goncalves.oliveira15@escola.pr.gov.br>
2026-02-24 09:48:26 -0300

    Add files via upload

---
+++ media.SE.js
@@ -0,0 +1,16 @@
+let aluno= "joao";
+let n1=99;
+let n2=100;
+let n3=99;
+let n4=100;
+let media=0;
+let result="";
+
+let soma = (n1 + n2 + n3 + n4)/4;
+
+if (media>=70){
+    result="aprovado"
+}else{
+    result="repovado"
+}
+console.log("O aluno"+aluno"tem a media"+media+"e está:"+result);
